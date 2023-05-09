33123a9cd902087f0dfab72874144835a7820360
Kosma <113530029+NVex0@users.noreply.github.com>
2023-05-09 21:25:18 +0700

    Update decoder.py

--- USBDecoder(Noob)/decoder.py
+++ USBDecoder(Noob)/decoder.py
@@ -1,62 +1,128 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import sys
 import os
 
-DataFileName = "usb.dat"
+mapping_shift = {
+  0x02: '<Shift>',
+  0x20: '<RShift>'
+}
 
-presses = []
+mapping = {
+  0x00: ["", ""],
+  0x01: ["ErrorRollOver", "ErrorRollOver"],
+  0x02: ["POSTFail9", "POSTFail9"],
+  0x03: ["ErrorUndefined", "ErrorUndefined"],
+  0x04: ["a", "A"],
+  0x05: ["b", "B"],
+  0x06: ["c", "C"],
+  0x07: ["d", "D"],
+  0x08: ["e", "E"],
+  0x09: ["f", "F"],
+  0x0A: ["g", "G"],
+  0x0B: ["h", "H"],
+  0x0C: ["i", "I"],
+  0x0D: ["j", "J"],
+  0x0E: ["k", "K"],
+  0x0F: ["l", "L"],
+  0x10: ["m", "M"],
+  0x11: ["n", "N"],
+  0x12: ["o", "O"],
+  0x13: ["p", "P"],
+  0x14: ["q", "Q"],
+  0x15: ["r", "R"],
+  0x16: ["s", "S"],
+  0x17: ["t", "T"],
+  0x18: ["u", "U"],
+  0x19: ["v", "V"],
+  0x1A: ["w", "W"],
+  0x1B: ["x", "X"],
+  0x1C: ["y", "Y"],
+  0x1D: ["z", "Z"],
+  0x1E: ["1", "!"],
+  0x1F: ["2", "@"],
+  0x20: ["3", "#"],
+  0x21: ["4", "$"],
+  0x22: ["5", "%"],
+  0x23: ["6", "^"],
+  0x24: ["7", "&"],
+  0x25: ["8", "*"],
+  0x26: ["9", "("],
+  0x27: ["0", ")"],
+  0x28: ["Return", "Return"],
+  0x29: ["ESCAPE", "ESCAPE"],
+  0x2A: ["<BACKSPACE>", "<BACKSPACE>"],
+  0x2B: ["Tab", "Tab"],
+  0x2C: [" ", " "],
+  # 0x2C: "Spacebar",
+  0x2D: ["-", "_"],
+  0x2E: ["=", "+"],
+  0x2F: ["[", "{"],
+  0x30: ["]", "}"],
+  0x31: ["\\", "|"],
+  0x32: ["Non-US"],
+  0x33: [";", ":"],
+  0x34: ["‘", "\""],
+  0x35: ["Grave"],
+  0x36: [",", "<"],
+  0x37: [".", ">"],
+  0x38: ["/", "?"],
+  0x39: ["Capslock", "Capslock"],
+  0x3A: ["F1", "F1"],
+  0x3B: ["F2", "F2"],
+  0x3C: ["F3", "F3"],
+  0x3D: ["F4", "F4"],
+  0x3E: ["F5", "F5"],
+  0x3F: ["F6", "F6"],
+  0x40: ["F7", "F7"],
+  0x41: ["F8", "F8"],
+  0x42: ["F9", "F9"],
+  0x43: ["F10", "F10"],
+  0x44: ["F11", "F11"],
+  0x45: ["F12", "F12"],
+  0x46: ["PrintScreen", "PrintScreen"],
+  0x47: ["ScrollLock", "ScrollLock"],
+  0x48: ["Pause", "Pause"],
+  0x49: ["Insert", "Insert"],
+  0x4A: ["Home", "Home"],
+  0x4B: ["PageUp", "PageUp"],
+  0x4C: ["DeleleForward", "DeleteForward"],
+  0x4D: ["End", "End"],
+  0x4E: ["PageDown", "PageDown"],
+  0x4F: ["RightArrow", "RightArrow"],
+  0x50: ["LeftArrow", "LeftArrow"]
+  # [OMITTED REST OF CODES FOR BREVITY]
+}
+infile = sys.argv[1]
+datasave = 'usbdataextracted'
 
-normalKeys = {"04":"a", "05":"b", "06":"c", "07":"d", "08":"e", "09":"f", "0a":"g", "0b":"h", "0c":"i", "0d":"j", "0e":"k", "0f":"l", "10":"m", "11":"n", "12":"o", "13":"p", "14":"q", "15":"r", "16":"s", "17":"t", "18":"u", "19":"v", "1a":"w", "1b":"x", "1c":"y", "1d":"z","1e":"1", "1f":"2", "20":"3", "21":"4", "22":"5", "23":"6","24":"7","25":"8","26":"9","27":"0","28":"<RET>","29":"<ESC>","2a":"<DEL>", "2b":"\t","2c":"<SPACE>","2d":"-","2e":"=","2f":"[","30":"]","31":"\\","32":"<NON>","33":";","34":"'","35":"<GA>","36":",","37":".","38":"/","39":"<CAP>","3a":"<F1>","3b":"<F2>", "3c":"<F3>","3d":"<F4>","3e":"<F5>","3f":"<F6>","40":"<F7>","41":"<F8>","42":"<F9>","43":"<F10>","44":"<F11>","45":"<F12>"}
+os.system(r"tshark -nr %s -Y 'len(usbhid.data) == 8' -T fields -e usbhid.data > %s" % (infile, datasave))
 
-shiftKeys = {"04":"A", "05":"B", "06":"C", "07":"D", "08":"E", "09":"F", "0a":"G", "0b":"H", "0c":"I", "0d":"J", "0e":"K", "0f":"L", "10":"M", "11":"N", "12":"O", "13":"P", "14":"Q", "15":"R", "16":"S", "17":"T", "18":"U", "19":"V", "1a":"W", "1b":"X", "1c":"Y", "1d":"Z","1e":"!", "1f":"@", "20":"#", "21":"$", "22":"%", "23":"^","24":"&","25":"*","26":"(","27":")","28":"<RET>","29":"<ESC>","2a":"<DEL>", "2b":"\t","2c":"<SPACE>","2d":"_","2e":"+","2f":"{","30":"}","31":"|","32":"<NON>","33":"\"","34":":","35":"<GA>","36":"<","37":">","38":"?","39":"<CAP>","3a":"<F1>","3b":"<F2>", "3c":"<F3>","3d":"<F4>","3e":"<F5>","3f":"<F6>","40":"<F7>","41":"<F8>","42":"<F9>","43":"<F10>","44":"<F11>","45":"<F12>"}
+with open (datasave, 'r') as f:
+  data = f.readlines()
 
-def main():
-    # check argv
-    if len(sys.argv) != 2:
-        print("Usage : ")
-        print("        python UsbKeyboardHacker.py data.pcap")
-        print("Tips : ")
-        print("        To use this python script , you must install the tshark first.")
-        print("        You can use `sudo apt-get install tshark` to install it")
-        print("Author : ")
-        print("        WangYihang <wangyihanger@gmail.com>")
-        print("        If you have any questions , please contact me by email.")
-        print("        Thank you for using.")
-        exit(1)
+result = ''
 
-    # get argv
-    pcapFilePath = sys.argv[1]
+for line in data:
+  interrupt_byte = int(line[6:8], 16)
+  shift_byte = int(line[0:2], 16)
+  shift_key = 0
+  # Remove the interrupted event, when two keys are pressed at the same time.
+  if interrupt_byte != 0x00:
+    continue
 
-    # get data of pcap
-    os.system("tshark -r %s -T fields -e usb.capdata 'usb.data_len == 8' > %s" % (pcapFilePath, DataFileName))
+  if shift_byte in mapping_shift:
+    shift_key = 1
 
-    # read data
-    with open(DataFileName, "r") as f:
-        for line in f:
-            presses.append(line[0:-1])
-    # handle
-    result = ""
-    for press in presses:
-        if press == '':
-            continue
-        if ':' in press:
-            Bytes = press.split(":")
-        else:
-            Bytes = [press[i:i+2] for i in range(0, len(press), 2)]
-        if Bytes[0] == "00":
-            if Bytes[2] != "00" and normalKeys.get(Bytes[2]):
-                result += normalKeys[Bytes[2]]
-        elif int(Bytes[0],16) & 0b10 or int(Bytes[0],16) & 0b100000: # shift key is pressed.
-            if Bytes[2] != "00" and normalKeys.get(Bytes[2]):
-                result += shiftKeys[Bytes[2]]
-        else:
-            print("[-] Unknow Key : %s" % (Bytes[0]))
-    print("[+] Found : %s" % (result))
+  normal_byte = int(line[4:6], 16)
 
-    # clean the temp data
-    os.system("rm ./%s" % (DataFileName))
+  if normal_byte in mapping:
+    stringed = mapping[normal_byte][shift_key]
 
-
-if __name__ == "__main__":
-    main()
+  if stringed == mapping[0x2A][0]:
+    # if backspace, remove one
+    result = result[:-1]
+  else:
+    result += stringed
+    
+print(result)
